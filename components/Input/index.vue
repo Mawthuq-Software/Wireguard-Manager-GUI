c978e84cc1c177064e7d10a0b5c0e47d5373fa57
Raspberry Tech <noobie.mine@gmail.com>
2021-12-04 14:38:34 +0000

    updated border

--- components/Input/index.vue
+++ components/Input/index.vue
@@ -1,52 +1,53 @@
 <template>
-	<div class="wg-input flex flex-col">
-		<label class="pb-2 leading-none">{{ label }}</label>
-		<input
-			:placeholder="placeholder"
-			:class="classes"
-			:type="type"
-			@change="inputData"
-		/>
-	</div>
+  <div class="wg-input flex flex-col">
+    <label class="pb-2 leading-none text-white">{{ label }}</label>
+    <input
+      :placeholder="placeholder"
+      :class="classes"
+      :type="type"
+      @change="inputData"
+    />
+  </div>
 </template>
 
 <script>
 export default {
-	name: "Input",
+  name: 'Input',
 
-	props: {
-		label: {
-			type: String,
-			default: "Label",
-		},
-		placeholder: {
-			type: String,
-			default: "Placeholder",
-		},
-		type: {
-			type: String,
-			validator: (value) => ["text", "password"].includes(value),
-			default: "password",
-		},
-	},
-	computed: {
-        classes () {
-            return {
-                "flex-1": true,
-				"border-gray-300": true,
-				rounded: true,
-				"py-2": true,
-				"px-4": true,
-				border: true,
-				"focus:border-blue-500": true,
-				"focus:outline-none": true,
-            }
-        }
+  props: {
+    label: {
+      type: String,
+      default: 'Label',
     },
-	methods: {
-		inputData(evt) {
-			this.$emit("dataChange", evt);
-		},
-	},
-};
+    placeholder: {
+      type: String,
+      default: 'Placeholder',
+    },
+    type: {
+      type: String,
+      validator: (value) => ['text', 'password'].includes(value),
+      default: 'password',
+    },
+  },
+  computed: {
+    classes() {
+      return {
+        'flex-1': true,
+        rounded: true,
+        'py-2': true,
+        'px-4': true,
+        border: true,
+        'border-background': true,
+        'focus:border-blue-500': true,
+        'focus:outline-none': true,
+        'bg-background': true,
+      }
+    },
+  },
+  methods: {
+    inputData(evt) {
+      this.$emit('dataChange', evt)
+    },
+  },
+}
 </script>
