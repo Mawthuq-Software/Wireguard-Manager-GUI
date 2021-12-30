850a1d27d72983a085fdb367827fc9438e32f943
Raspberry Tech <noobie.mine@gmail.com>
2021-12-30 16:42:33 +0000

    browser issue found to be the problem.

--- components/Checkbox/index.vue
+++ components/Checkbox/index.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="inline-flex items-center space-x-2">
-    <div class="flex">
-      <input
-        id="checkBox"
-        type="checkbox"
-        :checked="boxBool"
-        @change="checkData"
-      />
-    </div>
-    <div class="flex">
-      <label v-if="label != ''" for="checkBox" class="text-white">{{
-        label
-      }}</label>
-    </div>
+    <input
+      type="checkbox"
+      :checked="boxBool"
+      :class="inputClasses"
+      @change="checkData"
+    />
+    <p v-if="label != ''" class="text-white">{{label}}</p>
   </div>
 </template>
 <script>
@@ -33,18 +27,22 @@ export default {
       boxCheck: this.boxBool,
     }
   },
+  computed: {
+    inputClasses() {
+      return {
+        "h-4": true,
+        "w-4": true,
+        "rounded-sm": true,
+        "bg-secondary": true,
+        "text-white": true,
+      }
+    }
+  },
   methods: {
     checkData() {
       this.boxCheck = !this.boxCheck
       this.$emit('checkChange', this.boxCheck)
     },
-  },
+  }
 }
 </script>
-<style scoped>
-input {
-  height: 15px;
-  width: 15px;
-  border-radius: 2.5px;
-}
-</style>
