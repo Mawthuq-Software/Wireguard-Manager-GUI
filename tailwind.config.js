5d4a77e63b75c74c8a99138c1ac20ae35ab5eff4
Raspberry Tech <noobie.mine@gmail.com>
2022-01-09 16:55:35 +0000

    fixed broken changes

---
+++ tailwind.config.js
@@ -0,0 +1,77 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+
+    fontFamily: {
+      sans: ["'Source Sans Pro'"],
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#D6AD5B',
+          light: '#A37A29',
+          dark: '#CC9933',
+          grayscale: '#CC9933',
+          translucent: '#D6AD5B80',
+        },
+        secondary: {
+          DEFAULT: '#5C6AD7',
+          light: '#2836A4',
+          dark: '#3244CD',
+          grayscale: '#C2C7F0',
+          translucent: '#5C6AD780',
+        },
+        background: {
+          DEFAULT: '#202427',
+          light: '#2E3235',
+          ultralight: '#424649',
+        },
+        success: {
+          DEFAULT: '#5CD761',
+          translucent: '#5CD76180',
+        },
+        warning: {
+          DEFAULT: '#E18745',
+          light: '#DC7123',
+          dark: '#B05A1C',
+          grayscale: '#F4D4BD',
+          translucent: '#E1874580',
+        },
+        danger: {
+          DEFAULT: '#C24444',
+          translucent: '#C2444480',
+        },
+        inactive: {
+          DEFAULT: '#BFBFBF',
+        },
+      },
+    },
+  },
+  variants: {
+    backgroundColor: [
+      'responsive',
+      'group-hover',
+      'first',
+      'last',
+      'odd',
+      'even',
+      'hover',
+      'focus',
+      'active',
+      'visited',
+      'disabled',
+    ],
+    borderRadius: ['first', 'last'],
+    borderStyle: ['first', 'last'],
+    borderWidth: ['first', 'last'],
+    extend: {},
+  },
+  plugins: [],
+}
