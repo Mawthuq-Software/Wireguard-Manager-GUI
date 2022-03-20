7162b2e03aee0f90897d7b3d88aac15114d5682d
Raspberry Tech <noobie.mine@gmail.com>
2022-03-20 20:01:19 +0000

    Fv 45 fix tailwind defaults not loading in build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: {
+    enabled: false,
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
